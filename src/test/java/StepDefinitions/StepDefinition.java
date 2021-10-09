ea0f79e629306bccc59abfdc7f8d587110affd59
Punam <coolpoonu@gmail.com>
2021-10-09 13:33:02 +0530

    First

---
+++ src/test/java/StepDefinitions/StepDefinition.java
@@ -0,0 +1,25 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinition {
+
+	@Given("^User on landing page$")
+    public void user_on_landing_page() throws Throwable {
+        
+    }
+
+    @When("^User enters username & password and clicks on login button$")
+    public void user_enters_username_password_and_clicks_on_login_button() throws Throwable {
+
+    }
+
+    @Then("^Homepage should get displayed$")
+    public void homepage_should_get_displayed() throws Throwable {
+        
+    }
+
+	
+}
